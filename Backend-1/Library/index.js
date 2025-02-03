7f8e9d33ccedbcb7fcb99e36a68b7387f4cf1c3d
anjitha-0123 <anjithatv01@gmail.com>
2025-02-03 10:42:26 +0530

    update

--- Backend-1/Library/index.js
+++ Backend-1/Library/index.js
@@ -2,9 +2,14 @@ import express,{json} from "express";
 import dotenv from "dotenv";
 import {userauth} from "./Routes/userauth.js";
 import {adminauth} from "./Routes/adminauth.js"
+import cors from 'cors';
 
 const app=express();
 dotenv.config();
+app.use(cors({
+    origin: 'http://127.0.0.1:5500',    
+    credentials:true
+ }))
 
 app.listen(process.env.PORT,function(){
     console.log(`Server is listening at ${process.env.PORT}`)
